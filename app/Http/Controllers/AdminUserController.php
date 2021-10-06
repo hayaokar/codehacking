48ea0f6e5217504de9f91a4c892ff978816fcfd7
haya <princesshion@hotmail.com>
2021-10-06 23:23:10 +0300

    middleware not set

--- app/Http/Controllers/AdminUserController.php
+++ app/Http/Controllers/AdminUserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserEditRequest;
 use App\Http\Requests\UsersRequest;
 use App\Models\Photo;
 use App\Models\Role;
@@ -82,7 +83,11 @@ class AdminUserController extends Controller
      */
     public function edit($id)
     {
-        //
+        $user=User::findorfail($id);
+
+        $roles=Role::pluck('name','id')->toarray();
+
+        return view('admin.users.edit',compact('user','roles'));
     }
 
     /**
@@ -92,9 +97,35 @@ class AdminUserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UserEditRequest $request, $id)
     {
-        //
+        $user=User::findorfail($id);
+
+        $input=$request->all();
+
+        if(trim($request->password)==''){
+            $input=$request->except('password');
+        }
+        else{
+
+            $input=$request->all();
+            $input['password']=bcrypt($request->password);
+
+        }
+
+        if($file=$request->file('photo_id')){
+
+            $name=time(). $file->getClientOriginalName();
+
+            $file->move('images',$name);
+
+            $photo=Photo::create(['file'=>$name]);
+
+            $input['photo_id']=$photo->id;
+        }
+        $user->update($input);
+
+        return redirect('/admin/users');
     }
 
     /**
---
+++ resources/views/admin/users/edit.blade.php
@@ -0,0 +1,93 @@
+@extends('layouts.admin')
+
+
+@section('content')
+
+    <h1>Update users</h1>
+
+
+    <div class="row">
+
+        <div class="col-sm-3">
+
+            <img src="{{$user->photo ? $user->photo->file : 'http://placehold.it/400x400'}}" alt="" class="img-responsive img-rounded">
+
+        </div>
+
+
+        <div class="col-sm-9">
+
+            {!! Form::model($user,['method'=>'PATCH','action'=>['App\Http\Controllers\AdminUserController@update',$user->id],'files'=>true]) !!}
+
+
+
+            <div class="form-group">
+
+                {!!  Form::label('name', 'Name:')!!}
+                {!! Form::text('name',null,['class'=>'form-control']) !!}
+
+            </div>
+
+
+            <div class="form_group">
+
+                {!!  Form::label('email', 'Email:')!!}
+                {!! Form::email('email',null,['class'=>'form-control']) !!}
+
+            </div>
+
+
+            <div class="form_group">
+
+                {!!  Form::label('role_id', 'Role:')!!}
+                {!! Form::select('role_id',[''=>'Choose option']+$roles,null,['class'=>'form-control']) !!}
+
+            </div>
+
+            <div class="form_group">
+
+                {!!  Form::label('is_active', 'Status:')!!}
+                {!! Form::select('is_active',array(1=>'active',0=>'not active'),null,['class'=>'form-control']) !!}
+
+            </div>
+
+            <div class="form_group">
+
+                {!!  Form::label('photo_id', 'Photo')!!}
+                {!! Form::file('photo_id',null,['class'=>'form-control']) !!}
+
+            </div>
+
+            <div class="form_group">
+
+                {!!  Form::label('password', 'Password: ')!!}
+                {!! Form::password('password',['class'=>'form-control']) !!}
+
+            </div>
+
+
+            <div class="form_group">
+
+                {!! Form::submit('create user',['class'=>'btn btn-primary']) !!}
+
+            </div>
+
+
+
+            {!! Form::close() !!}
+
+
+        </div>
+
+    </div>
+
+
+
+
+    <div class="row">
+
+    @include('includes.form_errors')
+
+    </div>
+
+@stop
